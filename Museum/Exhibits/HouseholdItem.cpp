88f9344afaad03ba2565cbe506158512ff9e8fa7
terzi diana <d1ana4terz1@gmail.com>
2022-05-31 15:46:39 +0300

    Add field posibility of using in class HouseholdItem

--- Museum/Exhibits/HouseholdItem.cpp
+++ Museum/Exhibits/HouseholdItem.cpp
@@ -1,10 +1,14 @@
 #include "HouseholdItem.h"
 
-HouseholdItem::HouseholdItem(std::string author, std::string country, int year, double width, double length, double height):
-        VoluminousExhibit(author, country, year, width, length, height){}
+HouseholdItem::HouseholdItem(std::string author, std::string country, int year, double width, double length, double height, bool posibilityOfUsing):
+        VoluminousExhibit(author, country, year, width, length, height){
+    m_posibilityOfUsing = posibilityOfUsing;
+}
 
-HouseholdItem::HouseholdItem(std::string country, int year, double width, double length, double height):
-        VoluminousExhibit(country, year, width, length, height){}
+HouseholdItem::HouseholdItem(std::string country, int year, double width, double length, double height, bool posibilityOfUsing):
+        VoluminousExhibit(country, year, width, length, height){
+    m_posibilityOfUsing = posibilityOfUsing;
+}
 
 std::string HouseholdItem::Info() const{
     std::stringstream ss;
--- Museum/Exhibits/HouseholdItem.h
+++ Museum/Exhibits/HouseholdItem.h
@@ -5,9 +5,14 @@
 
 class HouseholdItem : public  VoluminousExhibit{
 public:
-    HouseholdItem(std::string author, std::string country, int year, double width, double length, double height);
-    HouseholdItem(std::string country, int year, double width, double length, double height);
+    HouseholdItem(std::string author, std::string country, int year, double width, double length,
+                  double height, bool  posibilityOfUsing);
+    HouseholdItem(std::string country, int year, double width, double length,
+                  double height, bool posibilityOfUsing);
     std::string Info() const override;
+
+private:
+    bool m_posibilityOfUsing;
 };
 
 #endif //HOUSEHOLDITEM_H
