c53fe2256445dd4d9f4eaad3c20d004e9233709c
terzi diana <d1ana4terz1@gmail.com>
2022-05-31 15:03:37 +0300

    Add field material in class Technics

--- Museum/Exhibits/Technics.cpp
+++ Museum/Exhibits/Technics.cpp
@@ -1,14 +1,18 @@
 #include "Technics.h"
 
-Technics::Technics(std::string author, std::string country, int year, double width, double length, double height) :
-        VoluminousExhibit(author, country, year, width, length, height){}
+Technics::Technics(std::string author, std::string country, int year, double width, double length, double height, std::string material) :
+        VoluminousExhibit(author, country, year, width, length, height){
+    m_material = material;
+}
 
 
-Technics::Technics(std::string country, int year, double width, double length, double height) :
-        VoluminousExhibit(country, year, width, length, height){}
+Technics::Technics(std::string country, int year, double width, double length, double height, std::string material) :
+        VoluminousExhibit(country, year, width, length, height){
+    m_material = material;
+}
 
 std::string Technics::Info() const{
     std::stringstream ss;
-    ss << VoluminousExhibit::Info() << "\nIt is a technics.";
+    ss << VoluminousExhibit::Info() << "\nIt is a technics.\nMaterial: " << m_material << '.';
     return ss.str();
 }
--- Museum/Exhibits/Technics.h
+++ Museum/Exhibits/Technics.h
@@ -5,9 +5,12 @@
 
 class Technics : public  VoluminousExhibit{
 public:
-    Technics(std::string author, std::string country, int year, double width, double length, double height);
-    Technics(std::string country, int year, double width, double length, double height);
+    Technics(std::string author, std::string country, int year, double width, double length, double height, std::string material);
+    Technics(std::string country, int year, double width, double length, double height, std::string material);
     std::string Info() const override;
+
+private:
+    std::string m_material;
 };
 
 #endif //TECHNICS_H
