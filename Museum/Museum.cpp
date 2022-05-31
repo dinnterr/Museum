33f76b41fd473423ae3deca900b87d26877d4cac
terzi diana <d1ana4terz1@gmail.com>
2022-06-01 00:43:57 +0300

    Add control class Museum

--- Museum/Museum.cpp
+++ Museum/Museum.cpp
@@ -1,1 +1,40 @@
 #include "Museum.h"
+
+Museum::~Museum() {
+    for (auto room: m_rooms){
+        delete room;
+    }
+    for (auto exhibit: m_exhibits){
+        delete exhibit;
+    }
+}
+
+void Museum::addRoom(Room *room) {
+    m_rooms.push_back(room);
+}
+
+void Museum::removeRoom(int pos) {
+    if (pos > m_rooms.size() || pos <= 0)
+    {
+        throw std::invalid_argument("Wrong number.");
+    }
+    else
+    {
+        m_rooms.erase(m_rooms.begin() + pos - 1);
+    }
+}
+
+void Museum::addExhibit(Exhibit *exhibit) {
+    m_exhibits.push_back(exhibit);
+}
+
+void Museum::removeExhibit(int pos) {
+    if (pos > m_exhibits.size() || pos <= 0)
+    {
+        throw std::invalid_argument("Wrong number.");
+    }
+    else
+    {
+        m_exhibits.erase(m_exhibits.begin() + pos - 1);
+    }
+}
--- Museum/Museum.h
+++ Museum/Museum.h
@@ -1,10 +1,34 @@
 #ifndef MUSEUM_H
 #define MUSEUM_H
 
+#include <iostream>
+#include <vector>
+#include "Rooms/Room.h"
+#include "Rooms/Windows.h"
+#include "Rooms/Doorway.h"
+#include "Exhibits/Exhibit.h"
+#include "Exhibits/Picture.h"
+#include "Exhibits/VoluminousExhibit.h"
+#include "Exhibits/Sculpture.h"
+#include "Exhibits/HouseholdItem.h"
+#include "Exhibits/Technics.h"
 
 class Museum {
+public:
+    Museum() = default;
+    ~Museum();
 
+    void addRoom (Room* room);
+    void removeRoom (int pos);
+
+    void addExhibit (Exhibit* exhibit);
+    void removeExhibit (int pos);
+
+
+
+private:
+    std::vector <Room*> m_rooms;
+    std::vector <Exhibit*> m_exhibits;
 };
-
 
 #endif //MUSEUM_H
