042af8b4462fbba95180c798bc32aea824f76ea0
terzi diana <d1ana4terz1@gmail.com>
2022-05-31 15:47:44 +0300

    Add field posibility of using in class HouseholdItem

--- Museum/Exhibits/HouseholdItem.h
+++ Museum/Exhibits/HouseholdItem.h
@@ -6,7 +6,7 @@
 class HouseholdItem : public  VoluminousExhibit{
 public:
     HouseholdItem(std::string author, std::string country, int year, double width, double length,
-                  double height, bool  posibilityOfUsing);
+                  double height, bool posibilityOfUsing);
     HouseholdItem(std::string country, int year, double width, double length,
                   double height, bool posibilityOfUsing);
     std::string Info() const override;
