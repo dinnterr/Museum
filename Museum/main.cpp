f48f5cc500e7242dc3b2dad6a3aabd19e371c869
terzi diana <d1ana4terz1@gmail.com>
2022-06-05 20:22:49 +0300

    Edit main

--- Museum/main.cpp
+++ Museum/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <stdexcept>
 #include <exception>
+#include <cstdlib>
 #include "Museum.h"
 #include "Rooms/Room.h"
 #include "Exhibits/Exhibit.h"
@@ -18,16 +19,52 @@ int main() {
     Museum museum;
     Room* room = nullptr;
     Exhibit* exhibit = nullptr;
-    /*while (true) {
-        cout << "Add rooms to museum." << endl;
+    std::vector <Windows> windows;
+    std::vector <Doorway> doorways;
+    cout << "Create a room of museum." << endl;
+    cout << "1. Add number of doorways." << endl;
+    int number_of_doorways;
+    cin >> number_of_doorways;
+    cout << "2. Add number of windows." << endl;
+    int number_of_windows;
+    cin >> number_of_windows;
+    double width, length, height;
+    cout << "3. Enter width of room." << endl;
+    cin >> width;
+    cout << "4. Enter length of room." << endl;
+    cin >> length;
+    cout << "5. Enter height of room." << endl;
+    cin >> height;
+    room = new Room(width,length,height,windows,doorways);
+    museum.addRoom(room);
 
-
-        try {
+    cout << "What do you want to do next?" << endl;
+    cout << "1. Add new room" << endl;
+    cout << "2. Add exhibit(-s)" << endl;
+    int n;
+    cin >> n;
+    switch (n) {
+        case 1: //Add room
+        {
+            cout << "1. Add number of doorways." << endl;
+            cin >> number_of_doorways;
+            cout << "2. Add number of windows." << endl;
+            cin >> number_of_windows;
+            cout << "3. Enter width of room." << endl;
+            cin >> width;
+            cout << "4. Enter length of room." << endl;
+            cin >> length;
+            cout << "5. Enter height of room." << endl;
+            cin >> height;
+            room = new Room(width, length, height, windows, doorways);
+            museum.addRoom(room);
+            break;
+        }
+        case 2: //Add exhibit
+        {
 
         }
-        catch (const exception &e) {
-            cerr << e.what() << endl;
-        }
-    }*/
+    }
+
     return 0;
 }
