4a5ee0198b561b80dcfb4dd41e4258f9b65292bc
terzi diana <d1ana4terz1@gmail.com>
2022-06-06 15:15:44 +0300

    Complete enums of extra fields

--- Museum/Exhibits/HouseholdItem.cpp
+++ Museum/Exhibits/HouseholdItem.cpp
@@ -1,13 +1,33 @@
 #include "HouseholdItem.h"
 
-HouseholdItem::HouseholdItem(const std::string& author, const std::string& name, const std::string& country, int year, double width, double length, double height, bool posibilityOfUsing):
+HouseholdItem::HouseholdItem(const std::string& author, const std::string& name, const std::string& country, int year, double width, double length, double height, int posibilityOfUsing):
         VoluminousExhibit(author, name, country, year, width, length, height){
-    m_posibilityOfUsing = posibilityOfUsing;
+    if(posibilityOfUsing < 0 || posibilityOfUsing > 1){
+        throw std::invalid_argument("Wrong number.");
+    }
+    switch (posibilityOfUsing) {
+        case usable: {
+            m_posibilityOfUsing = "Usable item.";
+        }
+        case notUsable:{
+            m_posibilityOfUsing = "Not usable item.";
+        }
+    }
 }
 
-HouseholdItem::HouseholdItem(const std::string& name, const std::string& country, int year, double width, double length, double height, bool posibilityOfUsing):
+HouseholdItem::HouseholdItem(const std::string& name, const std::string& country, int year, double width, double length, double height, int posibilityOfUsing):
         VoluminousExhibit(name, country, year, width, length, height){
-    m_posibilityOfUsing = posibilityOfUsing;
+    if(posibilityOfUsing < 0 || posibilityOfUsing > 1){
+        throw std::invalid_argument("Wrong number.");
+    }
+    switch (posibilityOfUsing) {
+        case usable: {
+            m_posibilityOfUsing = "Usable item.";
+        }
+        case notUsable:{
+            m_posibilityOfUsing = "Not usable item.";
+        }
+    }
 }
 
 std::string HouseholdItem::Info() const{
--- Museum/Exhibits/HouseholdItem.h
+++ Museum/Exhibits/HouseholdItem.h
@@ -3,18 +3,23 @@
 
 #include "VoluminousExhibit.h"
 
+enum Posibility{
+    usable,
+    notUsable
+};
+
 class HouseholdItem : public  VoluminousExhibit{
 public:
     HouseholdItem(const std::string& author, const std::string& name, const std::string& country, int year, double width, double length,
-                  double height, bool posibilityOfUsing);
+                  double height, int posibilityOfUsing);
     HouseholdItem(const std::string& name, const std::string& country, int year, double width, double length,
-                  double height, bool posibilityOfUsing);
+                  double height, int posibilityOfUsing);
     std::string Info() const override;
 
     void place(Room* room) override;
 
 private:
-    bool m_posibilityOfUsing;
+    std::string m_posibilityOfUsing;
 };
 
 #endif //HOUSEHOLDITEM_H
