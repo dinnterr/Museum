5da5f80b5c14305ed16fc40e33e70f5251f3250f
terzi diana <d1ana4terz1@gmail.com>
2022-06-06 14:42:49 +0300

    Add enum of materials

--- Museum/Exhibits/Technics.cpp
+++ Museum/Exhibits/Technics.cpp
@@ -1,16 +1,54 @@
 #include "Technics.h"
 
 Technics::Technics(const std::string& author, const std::string& name, const std::string& country, int year,
-                   double width, double length, double height, const std::string& material) :
+                   double width, double length, double height, int material) :
         VoluminousExhibit(author, name, country, year, width, length, height){
-    m_material = material;
+    switch(material){
+        case 1:{
+            m_material = "Stone";
+        }
+        case 2:{
+            m_material = "Iron";
+        }
+        case 3: {
+            m_material = "Aliminium";
+        }
+        case 4:{
+            m_material = "Plastic";
+        }
+        case 5:{
+            m_material = "Alloy";
+        }
+        case 6:{
+            m_material = "Rubber";
+        }
+    }
 }
 
 
 Technics::Technics(const std::string& name, const std::string& country, int year, double width, double length,
-                   double height, const std::string& material) :
+                   double height, int material) :
         VoluminousExhibit(name, country, year, width, length, height){
-    m_material = material;
+    switch(material){
+        case 1:{
+            m_material = "Stone";
+        }
+        case 2:{
+            m_material = "Iron";
+        }
+        case 3: {
+            m_material = "Aliminium";
+        }
+        case 4:{
+            m_material = "Plastic";
+        }
+        case 5:{
+            m_material = "Alloy";
+        }
+        case 6:{
+            m_material = "Rubber";
+        }
+    }
 }
 
 std::string Technics::Info() const{
--- Museum/Exhibits/Technics.h
+++ Museum/Exhibits/Technics.h
@@ -3,11 +3,20 @@
 
 #include "VoluminousExhibit.h"
 
+enum class Material {
+    stone = 1, //камень
+    iron,      //железо
+    aluminum,  //алюминий
+    plastic,   //пластик
+    alloy,      //сплав
+    rubber      //резина
+};
+
 class Technics : public  VoluminousExhibit{
 public:
     Technics(const std::string& author, const std::string& name, const std::string& country, int year, double width,
-             double length, double height, const std::string& material);
-    Technics(const std::string& name, const std::string& country, int year, double width, double length, double height, const std::string& material);
+             double length, double height, int material);
+    Technics(const std::string& name, const std::string& country, int year, double width, double length, double height, int material);
     std::string Info() const override;
 
     void place(Room* room) override;
