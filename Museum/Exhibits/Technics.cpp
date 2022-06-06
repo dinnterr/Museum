f004720e6836c3a62bb3e17b795fc20365b76d8e
terzi diana <d1ana4terz1@gmail.com>
2022-06-06 15:04:17 +0300

    Complete enum of materials

--- Museum/Exhibits/Technics.cpp
+++ Museum/Exhibits/Technics.cpp
@@ -8,22 +8,22 @@ Technics::Technics(const std::string& author, const std::string& name, const std
     }
     switch(material){
         case 1:{
-            m_material = "Stone";
+            m_material = "Stone.";
         }
         case 2:{
-            m_material = "Iron";
+            m_material = "Iron.";
         }
         case 3: {
-            m_material = "Aliminium";
+            m_material = "Aliminium.";
         }
         case 4:{
-            m_material = "Plastic";
+            m_material = "Plastic.";
         }
         case 5:{
-            m_material = "Alloy";
+            m_material = "Alloy.";
         }
         case 6:{
-            m_material = "Rubber";
+            m_material = "Rubber.";
         }
     }
 }
@@ -37,22 +37,22 @@ Technics::Technics(const std::string& name, const std::string& country, int year
     }
     switch(material){
         case 1:{
-            m_material = "Stone";
+            m_material = "Stone.";
         }
         case 2:{
-            m_material = "Iron";
+            m_material = "Iron.";
         }
         case 3: {
-            m_material = "Aliminium";
+            m_material = "Aliminium.";
         }
         case 4:{
-            m_material = "Plastic";
+            m_material = "Plastic.";
         }
         case 5:{
-            m_material = "Alloy";
+            m_material = "Alloy.";
         }
         case 6:{
-            m_material = "Rubber";
+            m_material = "Rubber.";
         }
     }
 }
