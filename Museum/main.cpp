9f8755fdddb2854e62628b0bb7f12c4c2f6af776
terzi diana <d1ana4terz1@gmail.com>
2022-06-06 16:50:09 +0300

    Edit main

--- Museum/main.cpp
+++ Museum/main.cpp
@@ -90,7 +90,7 @@ int main() {
                         {
                             string author, name, country;
                             int year;
-                            cout << "Enter author name." << endl;
+                            cout << "Enter the name of the exhibit's author" << endl;
                             cin >> author;
                             cout << "Enter exhibit name." << endl;
                             cin >> name;
@@ -108,15 +108,15 @@ int main() {
                             string author, name, country;
                             int year;
                             double p_width, p_height;
-                            cout << "Enter author name." << endl;
+                            cout << "Enter the name of the artist" << endl;
                             cin >> author;
-                            cout << "Enter exhibit name." << endl;
+                            cout << "Enter picture name." << endl;
                             cin >> name;
-                            cout << "Indicate the country of origin of the exhibit." << endl;
+                            cout << "Indicate the country of origin of the picture." << endl;
                             cin >> country;
-                            cout << "Enter the year the exhibit was created." << endl;
+                            cout << "Enter the year the picture was created." << endl;
                             cin >> year;
-                            cout << "Enter the width of the picture" << endl;
+                            cout << "Enter the width of the picture." << endl;
                             cin >> p_width;
                             cout << "Enter the height of the picture." << endl;
                             cin >> p_height;
@@ -127,28 +127,126 @@ int main() {
                         }
                         case 3: //Voluminous exhibit
                         {
-
+                            string author, name, country;
+                            int year;
+                            double v_width, v_height, v_length;
+                            cout << "Enter the name of the voluminous exhibit's author." << endl;
+                            cin >> author;
+                            cout << "Enter voluminous exhibit name." << endl;
+                            cin >> name;
+                            cout << "Indicate the country of origin of the voluminous exhibit." << endl;
+                            cin >> country;
+                            cout << "Enter the year the voluminous exhibit was created." << endl;
+                            cin >> year;
+                            cout << "Enter the width of the voluminous exhibit." << endl;
+                            cin >> v_width;
+                            cout << "Enter the length of the voluminous exhibit." << endl;
+                            cin >> v_length;
+                            cout << "Enter the height of the voluminous exhibit." << endl;
+                            cin >> v_height;
+                            exhibit = new VoluminousExhibit(author, name, country, year, v_width, v_length, v_height);
+                            museum.addExhibit(exhibit);
+                            cout << endl;
+                            break;
                         }
                         case 4: //Sculpture
                         {
-
+                            string author, name, country;
+                            int year, type;
+                            double s_width, s_height, s_length;
+                            cout << "Enter the name of the sculpture's author." << endl;
+                            cin >> author;
+                            cout << "Enter sculpture name." << endl;
+                            cin >> name;
+                            cout << "Indicate the country of origin of the sculpture." << endl;
+                            cin >> country;
+                            cout << "Enter the year the sculpture was created." << endl;
+                            cin >> year;
+                            cout << "Enter the width of the sculpture." << endl;
+                            cin >> s_width;
+                            cout << "Enter the length of the sculpture." << endl;
+                            cin >> s_length;
+                            cout << "Enter the height of the sculpture." << endl;
+                            cin >> s_height;
+                            cout << "Choose the type of sculpture:" << endl;
+                            cout << "1. Bust." << endl;
+                            cout << "2. Sculpture of small forms." << endl;
+                            cout << "3. Sculptural group." << endl;
+                            cout << "4. Statuette." << endl;
+                            cout << "5. Statue." << endl;
+                            cout << "6. Torso." << endl;
+                            cin >> type;
+                            exhibit = new Sculpture(author, name, country, year, s_width, s_length, s_height, type);
+                            museum.addExhibit(exhibit);
+                            cout << endl;
+                            break;
                         }
                         case 5: //Technics
                         {
-
+                            string author, name, country;
+                            int year, material;
+                            double t_width, t_height, t_length;
+                            cout << "Enter the name of the technic's author." << endl;
+                            cin >> author;
+                            cout << "Enter technic name." << endl;
+                            cin >> name;
+                            cout << "Indicate the country of origin of the technic." << endl;
+                            cin >> country;
+                            cout << "Enter the year the technic was created." << endl;
+                            cin >> year;
+                            cout << "Enter the width of the technic." << endl;
+                            cin >> t_width;
+                            cout << "Enter the length of the technic." << endl;
+                            cin >> t_length;
+                            cout << "Enter the height of the technic." << endl;
+                            cin >> t_height;
+                            cout << "Choose the material of technic:" << endl;
+                            cout << "1. Stone." << endl;
+                            cout << "2. Iron." << endl;
+                            cout << "3. Aluminium." << endl;
+                            cout << "4. Plastic." << endl;
+                            cout << "5. Alloy." << endl;
+                            cout << "6. Rubber." << endl;
+                            cin >> material;
+                            exhibit = new Technics(author, name, country, year, t_width, t_length, t_height, material);
+                            museum.addExhibit(exhibit);
+                            cout << endl;
+                            break;
                         }
                         case 6: //Household item
                         {
-
+                            string author, name, country;
+                            int year, posibilityOfUsing;
+                            double h_width, h_height, h_length;
+                            cout << "Enter the name of the household item's author." << endl;
+                            cin >> author;
+                            cout << "Enter household item name." << endl;
+                            cin >> name;
+                            cout << "Indicate the country of origin of the household item." << endl;
+                            cin >> country;
+                            cout << "Enter the year the household item was created." << endl;
+                            cin >> year;
+                            cout << "Enter the width of the household item." << endl;
+                            cin >> h_width;
+                            cout << "Enter the length of the household item." << endl;
+                            cin >> h_length;
+                            cout << "Enter the height of the household item." << endl;
+                            cin >> h_height;
+                            cout << "Choose the possibility of using the household item:" << endl;
+                            cout << "1. Usable item." << endl;
+                            cout << "2. Not usable item." << endl;
+                            cin >> posibilityOfUsing;
+                            exhibit = new HouseholdItem(author, name, country, year, h_width, h_length, h_height, posibilityOfUsing);
+                            museum.addExhibit(exhibit);
+                            cout << endl;
+                            break;
                         }
                     }
+                    break;
                 }
                 case 2: //Unknown author
                 {
-                    string name, country, sculpture_type, technics_material, posibilityOfUsing_householdItem;
-                    int year;
-                    double e_length, e_width, e_height;
-                    cout << "Okay. Select an exhibit type." << endl;
+                    cout << "Okay. Now select an exhibit type." << endl;
                     cout << "1. Simple exhibit (Author (if there is), name, country, year)." << endl;
                     cout << "2. Picture." << endl;
                     cout << "3. Voluminous exhibit (generalized)." << endl;
@@ -160,29 +258,148 @@ int main() {
                     cin >> n3;
                     cout << endl;
                     switch (n3) {
-                        case 1:
+                        case 1: //simple exhibit
                         {
-
+                            string name, country;
+                            int year;
+                            cout << "Enter exhibit name." << endl;
+                            cin >> name;
+                            cout << "Indicate the country of origin of the exhibit." << endl;
+                            cin >> country;
+                            cout << "Enter the year the exhibit was created." << endl;
+                            cin >> year;
+                            exhibit = new Exhibit(name, country, year);
+                            museum.addExhibit(exhibit);
+                            cout << endl;
+                            break;
                         }
-                        case 2:
+                        case 2: //Picture
                         {
-
+                            string name, country;
+                            int year;
+                            double p_width, p_height;
+                            cout << "Enter picture name." << endl;
+                            cin >> name;
+                            cout << "Indicate the country of origin of the picture." << endl;
+                            cin >> country;
+                            cout << "Enter the year the picture was created." << endl;
+                            cin >> year;
+                            cout << "Enter the width of the picture." << endl;
+                            cin >> p_width;
+                            cout << "Enter the height of the picture." << endl;
+                            cin >> p_height;
+                            exhibit = new Picture(name, country, year, p_width, p_height);
+                            museum.addExhibit(exhibit);
+                            cout << endl;
+                            break;
                         }
-                        case 3:
+                        case 3: //Voluminous exhibit
                         {
-
+                            string name, country;
+                            int year;
+                            double v_width, v_height, v_length;
+                            cout << "Enter voluminous exhibit name." << endl;
+                            cin >> name;
+                            cout << "Indicate the country of origin of the voluminous exhibit." << endl;
+                            cin >> country;
+                            cout << "Enter the year the voluminous exhibit was created." << endl;
+                            cin >> year;
+                            cout << "Enter the width of the voluminous exhibit." << endl;
+                            cin >> v_width;
+                            cout << "Enter the length of the voluminous exhibit." << endl;
+                            cin >> v_length;
+                            cout << "Enter the height of the voluminous exhibit." << endl;
+                            cin >> v_height;
+                            exhibit = new VoluminousExhibit(name, country, year, v_width, v_length, v_height);
+                            museum.addExhibit(exhibit);
+                            cout << endl;
+                            break;
                         }
-                        case 4:
+                        case 4: //Sculpture
                         {
-
+                            string name, country;
+                            int year, type;
+                            double s_width, s_height, s_length;
+                            cout << "Enter sculpture name." << endl;
+                            cin >> name;
+                            cout << "Indicate the country of origin of the sculpture." << endl;
+                            cin >> country;
+                            cout << "Enter the year the sculpture was created." << endl;
+                            cin >> year;
+                            cout << "Enter the width of the sculpture." << endl;
+                            cin >> s_width;
+                            cout << "Enter the length of the sculpture." << endl;
+                            cin >> s_length;
+                            cout << "Enter the height of the sculpture." << endl;
+                            cin >> s_height;
+                            cout << "Choose the type of sculpture:" << endl;
+                            cout << "1. Bust." << endl;
+                            cout << "2. Sculpture of small forms." << endl;
+                            cout << "3. Sculptural group." << endl;
+                            cout << "4. Statuette." << endl;
+                            cout << "5. Statue." << endl;
+                            cout << "6. Torso." << endl;
+                            cin >> type;
+                            exhibit = new Sculpture(name, country, year, s_width, s_length, s_height, type);
+                            museum.addExhibit(exhibit);
+                            cout << endl;
+                            break;
                         }
-                        case 5:
+                        case 5: //Technics
                         {
-
+                            string name, country;
+                            int year, material;
+                            double t_width, t_height, t_length;
+                            cout << "Enter technic name." << endl;
+                            cin >> name;
+                            cout << "Indicate the country of origin of the technic." << endl;
+                            cin >> country;
+                            cout << "Enter the year the technic was created." << endl;
+                            cin >> year;
+                            cout << "Enter the width of the technic." << endl;
+                            cin >> t_width;
+                            cout << "Enter the length of the technic." << endl;
+                            cin >> t_length;
+                            cout << "Enter the height of the technic." << endl;
+                            cin >> t_height;
+                            cout << "Choose the material of technic:" << endl;
+                            cout << "1. Stone." << endl;
+                            cout << "2. Iron." << endl;
+                            cout << "3. Aluminium." << endl;
+                            cout << "4. Plastic." << endl;
+                            cout << "5. Alloy." << endl;
+                            cout << "6. Rubber." << endl;
+                            cin >> material;
+                            exhibit = new Technics(name, country, year, t_width, t_length, t_height, material);
+                            museum.addExhibit(exhibit);
+                            cout << endl;
+                            break;
                         }
-                        case 6:
+                        case 6: //Household item
                         {
-
+                            string name, country;
+                            int year, possibilityOfUsing;
+                            double h_width, h_height, h_length;
+                            cout << "Enter household item name." << endl;
+                            cin >> name;
+                            cout << "Indicate the country of origin of the household item." << endl;
+                            cin >> country;
+                            cout << "Enter the year the household item was created." << endl;
+                            cin >> year;
+                            cout << "Enter the width of the household item." << endl;
+                            cin >> h_width;
+                            cout << "Enter the length of the household item." << endl;
+                            cin >> h_length;
+                            cout << "Enter the height of the household item." << endl;
+                            cin >> h_height;
+                            cout << "Choose the possibility of using the household item:" << endl;
+                            cout << "1. Usable item." << endl;
+                            cout << "2. Not usable item." << endl;
+                            cin >> possibilityOfUsing;
+                            exhibit = new HouseholdItem(name, country, year, h_width, h_length, h_height, possibilityOfUsing);
+                            museum.addExhibit(exhibit);
+                            cout << endl;
+                            break;
                         }
                     }
                     break;
